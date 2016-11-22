aed33cd4f67d768d078bcb9e85e9e77ff5a14d6a
Jake Birchall <drwhat@cryto.net>
2016-11-22 02:54:06 +0000

    Revert Last change

--- TeamSpeak3/Transport/AbstractTransport.php
+++ TeamSpeak3/Transport/AbstractTransport.php
@@ -191,7 +191,7 @@ abstract class AbstractTransport
      */
     public function setAdapter(AbstractAdapter $adapter)
     {
-        $this->adapter = get_class($adapter);
+        $this->adapter = $adapter;
     }
 
     /**
@@ -212,7 +212,7 @@ abstract class AbstractTransport
     public function getAdapterType()
     {
         if ($this->adapter instanceof AbstractAdapter) {
-            $string = StringHelper::factory($this->adapter);
+            $string = StringHelper::factory(get_class($this->adapter));
 
             return $string->substr($string->findLast("\\"))->replace(array("\\", " "), "")->toString();
         }
